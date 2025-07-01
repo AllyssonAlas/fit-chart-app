705f0f897eae9268c0a48ab68ec5d2602adb4242
Allysson Alas <allyssonmgs@gmail.com>
2025-07-01 12:52:59 -0300

    test: ensure AxiosHttpClient calls axios.request() with correct input

---
+++ tests/infra/gateways/axios-http-client.spec.ts
@@ -0,0 +1,35 @@
+import axios from 'axios';
+
+import { AxiosHttpClient } from '@/infra/gateways/';
+
+jest.mock('axios');
+
+describe('AxiosHttpClient', () => {
+  const input = {
+    url: 'any_url',
+    method: 'post',
+    params: { any: 'any' },
+  };
+
+  let sut: AxiosHttpClient;
+  let fakeAxios: jest.Mocked<typeof axios>;
+
+  beforeAll(() => {
+    fakeAxios = axios as jest.Mocked<typeof axios>;
+  });
+
+  beforeEach(() => {
+    sut = new AxiosHttpClient();
+  });
+
+  it('Should call request with correct input', async () => {
+    await sut.request(input);
+
+    expect(fakeAxios.request).toHaveBeenCalledWith({
+      url: 'any_url',
+      method: 'post',
+      params: { any: 'any' },
+    });
+    expect(fakeAxios.request).toHaveBeenCalledTimes(1);
+  });
+});
