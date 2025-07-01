74c94df7bdfa9641ae2d4cebd2e9d8a157bdd8b8
Allysson Alas <allyssonmgs@gmail.com>
2025-07-01 15:11:29 -0300

    feat: ensure AxiosHttpClient returns correct output on success

--- src/infra/gateways/axios-http-client.ts
+++ src/infra/gateways/axios-http-client.ts
@@ -2,8 +2,9 @@ import axios from 'axios';
 
 import type { HttpClient } from '@/domain/contracts/gateways';
 
-export class AxiosHttpClient {
-  async request(input: HttpClient.Input): Promise<void> {
-    await axios.request(input);
+export class AxiosHttpClient implements HttpClient {
+  async request(input: HttpClient.Input): Promise<HttpClient.Output> {
+    const { data, status } = await axios.request(input);
+    return { statusCode: status, body: data };
   }
 }
