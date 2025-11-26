fedc03a95720a71b1ec245655e097d49a3a44389
Allysson Alas <allyssonmgs@gmail.com>
2025-11-26 17:54:40 -0300

    test: ensure ValidationComposite returns correct error object on first validation failure

---
+++ tests/presentation/validation/composite.spec.ts
@@ -0,0 +1,41 @@
+import { type MockProxy, mock } from 'jest-mock-extended';
+import type { Validator } from '@/presentation/protocols';
+import { ValidationComposite } from '@/presentation/validation';
+
+describe('ValidationComposite', () => {
+  let sut: ValidationComposite;
+  let validator1: MockProxy<Validator>;
+  let validator2: MockProxy<Validator>;
+  let validator3: MockProxy<Validator>;
+  let validators: MockProxy<Validator>[];
+
+  beforeEach(() => {
+    validator1 = mock();
+    validator2 = mock();
+    validator3 = mock();
+    validators = [validator1, validator2, validator3].map((validatorMock) => {
+      validatorMock.field = 'any_field';
+      validatorMock.validate.mockReturnValue(undefined);
+      return validatorMock;
+    });
+
+    sut = new ValidationComposite(validators);
+  });
+
+  it('Should return the error if the first validation failed', () => {
+    const mockedError = new Error('any_error');
+    const mockedError2 = new Error('any_error_2');
+    validators[1].validate.mockReturnValueOnce({
+      field: 'any_field',
+      error: mockedError,
+    });
+    validators[2].validate.mockReturnValueOnce({
+      field: 'any_field',
+      error: mockedError2,
+    });
+
+    const error = sut.validate({ any_field: 'any_value' });
+
+    expect(error).toEqual({ field: 'any_field', error: mockedError });
+  });
+});
