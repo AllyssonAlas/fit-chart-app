7851f7e75bb05638087545e178e7676e3aa95ef7
Allysson Alas <allyssonmgs@gmail.com>
2025-12-04 14:34:45 -0300

    test: ensure SignUp presents errors if validation fails

--- tests/presentation/screens/SignUp.spec.tsx
+++ tests/presentation/screens/SignUp.spec.tsx
@@ -1,11 +1,13 @@
-import { render, screen } from '@testing-library/react-native';
+import { fireEvent, render, screen } from '@testing-library/react-native';
+import { mock } from 'jest-mock-extended';
 import React from 'react';
-
+import type { Validation } from '@/presentation/protocols';
 import { SignUp } from '@/presentation/screens/SignUp';
 
 describe('SignUp', () => {
   it('Should start with correct initial state', () => {
-    render(<SignUp />);
+    const validationMock = mock<Validation>();
+    render(<SignUp validation={validationMock} />);
 
     const nameInputError = screen.queryByTestId('name-input-error');
     const emailInputError = screen.queryByTestId('email-input-error');
@@ -22,5 +24,48 @@ describe('SignUp', () => {
     expect(confirmPasswordInputError).toBeNull();
     expect(rolePickerError).toBeNull();
     expect(submitButton).toBeDisabled();
+  });
+
+  it('Should present validation errors if validation fails', () => {
+    const validationMock = mock<Validation>();
+    validationMock.validate.mockReturnValue([
+      { field: 'name', error: 'any_name_error' },
+      { field: 'email', error: 'any_email_error' },
+      { field: 'password', error: 'any_password_error' },
+      { field: 'confirmPassword', error: 'any_confirm-password_error' },
+      { field: 'role', error: 'any_role_error' },
+    ]);
+    render(<SignUp validation={validationMock} />);
+
+    const nameInput = screen.queryByTestId('name-input');
+    fireEvent.changeText(nameInput, '123');
+    const emailInput = screen.queryByTestId('email-input');
+    fireEvent.changeText(emailInput, '123');
+    const passwordInput = screen.queryByTestId('password-input');
+    fireEvent.changeText(passwordInput, '123');
+    const confirmPasswordInput = screen.queryByTestId('confirm-password-input');
+    fireEvent.changeText(confirmPasswordInput, '123');
+    fireEvent(
+      screen.queryByTestId('role-picker'),
+      'onValueChange',
+      'any_value',
+    );
+    const submitButton = screen.getByTestId('submit-button');
+    fireEvent.press(submitButton);
+    const nameInputError = screen.queryByTestId('name-input-error');
+    const emailInputError = screen.queryByTestId('email-input-error');
+    const passwordInputError = screen.queryByTestId('password-input-error');
+    const confirmPasswordInputError = screen.queryByTestId(
+      'confirm-password-input-error',
+    );
+    const rolePickerError = screen.queryByTestId('role-picker-error');
+
+    expect(nameInputError).toHaveTextContent('any_name_error');
+    expect(emailInputError).toHaveTextContent('any_email_error');
+    expect(passwordInputError).toHaveTextContent('any_password_error');
+    expect(confirmPasswordInputError).toHaveTextContent(
+      'any_confirm-password_error',
+    );
+    expect(rolePickerError).toHaveTextContent('any_role_error');
   });
 });
