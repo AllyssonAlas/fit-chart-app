c5656ec150cbd7d9b0c3b090942d51ab04798842
Allysson Alas <allyssonmgs@gmail.com>
2025-12-17 20:36:19 -0300

    test: ensure Login presents errors if validation fails

--- tests/presentation/screens/Login.spec.tsx
+++ tests/presentation/screens/Login.spec.tsx
@@ -1,14 +1,33 @@
-import { render, screen } from '@testing-library/react-native';
+import {
+  fireEvent,
+  render,
+  screen,
+} from '@testing-library/react-native';
+import { type MockProxy, mock } from 'jest-mock-extended';
 // biome-ignore lint/correctness/noUnusedImports: React is required for JSX
 import React from 'react';
 
+import type { Validation } from '@/presentation/protocols';
 import { Login as LoginScreen } from '@/presentation/screens/Login';
 
-import { checkInputError } from '@/tests/presentation/utils/test-helpers';
+import {
+  checkInputError,
+  populateInput,
+} from '@/tests/presentation/utils/test-helpers';
+
+const simulateSubmitForm = () => {
+  populateInput('email', 'any_email');
+  populateInput('password', 'any_password');
+  const submitButton = screen.getByTestId('submit-button');
+  fireEvent.press(submitButton);
+};
 
 describe('Login', () => {
+  let validation: MockProxy<Validation>;
+
   beforeEach(() => {
-    render(<LoginScreen />);
+    validation = mock();
+    render(<LoginScreen validation={validation} />);
   });
 
   it('Should start with correct initial state', () => {
@@ -17,5 +36,17 @@ describe('Login', () => {
     checkInputError('email-input', '', false);
     checkInputError('password-input', '', false);
     expect(submitButton).toBeDisabled();
+  });
+
+  it('Should present validation errors if validation fails', () => {
+    validation.validate.mockReturnValueOnce([
+      { field: 'email', error: 'any_email_error' },
+      { field: 'password', error: 'any_password_error' },
+    ]);
+
+    simulateSubmitForm();
+
+    checkInputError('email-input', 'any_email_error');
+    checkInputError('password-input', 'any_password_error');
   });
 });
