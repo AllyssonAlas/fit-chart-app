5a5f6a811cac3f4b88c66b90dfbde499238cfeed
Allysson Alas <allyssonmgs@gmail.com>
2025-12-17 20:02:40 -0300

    feat: ensure Login starts with correct state

--- src/presentation/screens/Login/index.tsx
+++ src/presentation/screens/Login/index.tsx
@@ -1,5 +1,5 @@
 // biome-ignore lint/correctness/noUnusedImports: React is required for JSX
-import React from 'react';
+import React, { useState } from 'react';
 import { Text, View } from 'react-native';
 import { SafeAreaView } from 'react-native-safe-area-context';
 
@@ -8,6 +8,11 @@ import { Button, Input } from '@/presentation/components';
 import { styles } from './styles';
 
 export const Login = () => {
+  const [state] = useState({
+    emailError: '',
+    passwordError: '',
+  });
+
   return (
     <SafeAreaView style={styles.safeArea}>
       <View style={styles.container}>
@@ -20,20 +25,24 @@ export const Login = () => {
           </View>
           <View style={styles.formSection}>
             <Input
+              error={state.emailError}
               icon={'email'}
               keyboardType={'email-address'}
               label={'Email'}
+              name={'email'}
               placeholder={'Digite seu email'}
             />
             <Input
+              error={state.passwordError}
               icon={'lock'}
               isPasswordInput
               keyboardType={'visible-password'}
               label={'Senha'}
+              name={'password'}
               placeholder={'Digite sua senha'}
             />
           </View>
-          <Button title={'Entrar'} />
+          <Button disabled title={'Entrar'} />
           <View style={styles.signUpSection}>
             <Text style={styles.signUpText}>Não tem uma conta? </Text>
             <Text style={styles.signUpLink}>Cadastre-se</Text>
