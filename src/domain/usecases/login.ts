7e34cbf6fbb5313af0154380148eb060ebceb609
Allysson Alas <allyssonmgs@gmail.com>
2025-12-17 17:13:49 -0300

    feat: ensure Login returns correct output if HttpClient returns 200

--- src/domain/usecases/login.ts
+++ src/domain/usecases/login.ts
@@ -5,18 +5,20 @@ type Input = {
   email: string;
   password: string;
 };
-
-export type Login = (input: Input) => Promise<void>;
+type Output = { name: string; email: string; accessToken: string };
+export type Login = (input: Input) => Promise<Output>;
 type Setup = (url: string, httpClient: HttpClient) => Login;
 
 export const setupLogin: Setup = (url, httpClient) => {
   return async (input) => {
-    const { statusCode } = await httpClient.request({
+    const { statusCode, body } = await httpClient.request({
       url,
       method: 'post',
       body: input,
     });
     switch (statusCode) {
+      case HttpStatusCode.ok:
+        return body;
       case HttpStatusCode.unauthorized:
         throw new InvalidCredentialsError();
       default:
