1434f64006ba19eb506fa7fb444a019ae1646f4d
Allysson Alas <allyssonmgs@gmail.com>
2025-12-21 16:14:55 -0300

    chore: remove App.tsx from coverage

--- jest.config.js
+++ jest.config.js
@@ -4,6 +4,7 @@ module.exports = {
     '<rootDir>/src/**/*.{ts,tsx}',
     '!<rootDir>/src/main/**',
     '!<rootDir>/src/**/index.ts',
+    '!<rootDir>/src/App.tsx',
   ],
   coverageDirectory: "coverage",
   coverageProvider: "babel",
