52de55bff5026b45f1901e693b7cdca68c4355bb
Allysson Alas <allyssonmgs@gmail.com>
2026-01-02 17:42:40 -0300

    test: ensure LoadUserCurrentFitChart throws ServerError if HttpClient returns 500

--- tests/domain/usecases/load-user-current-fit-chart.spec.ts
+++ tests/domain/usecases/load-user-current-fit-chart.spec.ts
@@ -1,6 +1,7 @@
 import { type MockProxy, mock } from 'jest-mock-extended';
 
-import type { HttpClient } from '@/domain/contracts/gateways';
+import { type HttpClient, HttpStatusCode } from '@/domain/contracts/gateways';
+import { UnexpectedError } from '@/domain/errors';
 
 import {
   type LoadUserCurrentFitChart,
@@ -15,6 +16,9 @@ describe('LoadUserCurrentFitChart', () => {
 
   beforeAll(() => {
     httpClient = mock();
+    httpClient.request.mockResolvedValue({
+      statusCode: HttpStatusCode.ok,
+    });
   });
 
   beforeEach(() => {
@@ -29,5 +33,15 @@ describe('LoadUserCurrentFitChart', () => {
       method: 'get',
     });
     expect(httpClient.request).toHaveBeenCalledTimes(1);
+  });
+
+  it('Should throw UnexpectedError if HttpClient returns 500', async () => {
+    httpClient.request.mockResolvedValueOnce({
+      statusCode: HttpStatusCode.serverError,
+    });
+
+    const promise = sut();
+
+    await expect(promise).rejects.toThrow(new UnexpectedError());
   });
 });
