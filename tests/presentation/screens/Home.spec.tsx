cc3e82b7be7a50e9bbd4081794bfef420a4768ed
Allysson Alas <allyssonmgs@gmail.com>
2026-01-05 17:32:23 -0300

    test: ensure HomeScreen calls LoadUserCurrentFitChart once on start

--- tests/presentation/screens/Home.spec.tsx
+++ tests/presentation/screens/Home.spec.tsx
@@ -5,13 +5,20 @@ import React from 'react';
 import { Home } from '@/presentation/screens/Home';
 
 describe('Home', () => {
+  let loadUserCurrentFitChartUsecase: jest.Mock;
+
   beforeEach(() => {
-    render(<Home />);
+    loadUserCurrentFitChartUsecase = jest.fn();
+    render(<Home loadUserCurrentFitChart={loadUserCurrentFitChartUsecase} />);
   });
 
   it('Should start with correct initial state', () => {
     const loadingIndicator = screen.getByTestId('loading-indicator');
 
     expect(loadingIndicator).toBeTruthy();
+  });
+
+  it('Should LoadUserCurrentFitChart usecase on start', () => {
+    expect(loadUserCurrentFitChartUsecase).toHaveBeenCalledTimes(1);
   });
 });
