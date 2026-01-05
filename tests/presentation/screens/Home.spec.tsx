da97b7f0ef47e8a44485d079e618a3e954e0a836
Allysson Alas <allyssonmgs@gmail.com>
2026-01-05 18:55:18 -0300

    refactor: move sut creation to a factory method

--- tests/presentation/screens/Home.spec.tsx
+++ tests/presentation/screens/Home.spec.tsx
@@ -4,58 +4,63 @@ import React from 'react';
 import { UnexpectedError } from '@/domain/errors';
 import { Home } from '@/presentation/screens/Home';
 
+const makeSut = (
+  loadUserCurrentFitChartUsecase = jest.fn().mockResolvedValue({
+    id: 'any_fit_chart_id',
+    userId: 'any_user_id',
+    goals: 'any_goal',
+    observation: 'any_observation',
+    divisions: [
+      { name: 'any_division_1', weekDays: [0, 1] },
+      { name: 'any_division_2', weekDays: [2, 3] },
+    ],
+    exercises: [
+      {
+        exerciseId: 'any_exercise_id_1',
+        series: 4,
+        repts: 12,
+        weight: 20,
+        division: 'any_division_1',
+      },
+      {
+        exerciseId: 'any_exercise_id_2',
+        series: 3,
+        repts: 10,
+        weight: 30,
+        division: 'any_division_2',
+      },
+    ],
+  }),
+) => {
+  render(<Home loadUserCurrentFitChart={loadUserCurrentFitChartUsecase} />);
+  return { loadUserCurrentFitChartUsecase };
+};
+
 describe('Home', () => {
-  let loadUserCurrentFitChartUsecase: jest.Mock;
-
-  beforeEach(() => {
-    loadUserCurrentFitChartUsecase = jest.fn();
-    loadUserCurrentFitChartUsecase.mockResolvedValue({
-      id: 'any_fit_chart_id',
-      userId: 'any_user_id',
-      goals: 'any_goal',
-      observation: 'any_observation',
-      divisions: [
-        { name: 'any_division_1', weekDays: [0, 1] },
-        { name: 'any_division_2', weekDays: [2, 3] },
-      ],
-      exercises: [
-        {
-          exerciseId: 'any_exercise_id_1',
-          series: 4,
-          repts: 12,
-          weight: 20,
-          division: 'any_division_1',
-        },
-        {
-          exerciseId: 'any_exercise_id_2',
-          series: 3,
-          repts: 10,
-          weight: 30,
-          division: 'any_division_2',
-        },
-      ],
-    });
-    render(<Home loadUserCurrentFitChart={loadUserCurrentFitChartUsecase} />);
-  });
-
   it('Should start with correct initial state', () => {
+    makeSut();
+
     const loadingIndicator = screen.getByTestId('loading-indicator');
 
     expect(loadingIndicator).toBeTruthy();
   });
 
   it('Should LoadUserCurrentFitChart usecase on start', () => {
+    const { loadUserCurrentFitChartUsecase } = makeSut();
+
     expect(loadUserCurrentFitChartUsecase).toHaveBeenCalledTimes(1);
   });
 
   it('Should present error message if LoadUserCurrentFitChart throws UnexpectedError', async () => {
-    loadUserCurrentFitChartUsecase.mockRejectedValueOnce(new UnexpectedError());
-
-    render(<Home loadUserCurrentFitChart={loadUserCurrentFitChartUsecase} />);
+    const loadUserCurrentFitChartUsecase = jest
+      .fn()
+      .mockRejectedValueOnce(new UnexpectedError());
+    makeSut(loadUserCurrentFitChartUsecase);
 
     const errorText = await screen.findByTestId('error-message');
-    expect(errorText).toHaveTextContent('Erro ao carregar ficha de treino.');
     const errorButton = await screen.findByTestId('error-button');
+
+    expect(errorText).toHaveTextContent('Erro ao carregar ficha de treino.');
     expect(errorButton).toBeTruthy();
   });
 });
