fda021044a0280db1a37dc71658e92b9b99c3fe7
Allysson Alas <allyssonmgs@gmail.com>
2026-02-23 18:46:38 -0300

    test: ensure Home presents correct message when there is no exercises for division of the day

--- tests/domain/usecases/load-user-current-fit-chart.spec.ts
+++ tests/domain/usecases/load-user-current-fit-chart.spec.ts
@@ -2,8 +2,9 @@ import { type MockProxy, mock } from 'jest-mock-extended';
 
 import { type HttpClient, HttpStatusCode } from '@/domain/contracts/gateways';
 import { UnexpectedError } from '@/domain/errors';
-
 import { type LoadUserCurrentFitChart, setupLoadUserCurrentFitChart } from '@/domain/usecases';
+
+import { mockFitChart } from '@/tests/mocks/domain/entitites';
 
 describe('LoadUserCurrentFitChart', () => {
   const url = 'any_url';
@@ -15,37 +16,7 @@ describe('LoadUserCurrentFitChart', () => {
     httpClient = mock();
     httpClient.request.mockResolvedValue({
       statusCode: HttpStatusCode.ok,
-      body: {
-        id: 'any_fit_chart_id',
-        userId: 'any_user_id',
-        goals: 'any_goal',
-        observation: 'any_observation',
-        divisions: [
-          { name: 'any_division_1', weekDays: [0, 1] },
-          { name: 'any_division_2', weekDays: [2, 3] },
-        ],
-        exercises: [
-          {
-            exerciseId: 'any_exercise_id_1',
-            name: 'any_exercise_name_1',
-            equipment: 'any_equipment_1',
-            series: 4,
-            repts: 10,
-            weight: 20,
-            division: 'any_division_1',
-            category: 'any_category_1',
-          },
-          {
-            exerciseId: 'any_exercise_id_2',
-            name: 'any_exercise_name_2',
-            series: 3,
-            repts: 12,
-            weight: 24,
-            division: 'any_division_1',
-            category: 'any_category_1',
-          },
-        ],
-      },
+      body: mockFitChart(),
     });
   });
 
@@ -86,36 +57,6 @@ describe('LoadUserCurrentFitChart', () => {
   it('Should return correct output on success', async () => {
     const result = await sut();
 
-    expect(result).toEqual({
-      id: 'any_fit_chart_id',
-      userId: 'any_user_id',
-      goals: 'any_goal',
-      observation: 'any_observation',
-      divisions: [
-        { name: 'any_division_1', weekDays: [0, 1] },
-        { name: 'any_division_2', weekDays: [2, 3] },
-      ],
-      exercises: [
-        {
-          exerciseId: 'any_exercise_id_1',
-          name: 'any_exercise_name_1',
-          equipment: 'any_equipment_1',
-          series: 4,
-          repts: 10,
-          weight: 20,
-          division: 'any_division_1',
-          category: 'any_category_1',
-        },
-        {
-          exerciseId: 'any_exercise_id_2',
-          name: 'any_exercise_name_2',
-          series: 3,
-          repts: 12,
-          weight: 24,
-          division: 'any_division_1',
-          category: 'any_category_1',
-        },
-      ],
-    });
+    expect(result).toEqual(mockFitChart());
   });
 });
--- tests/presentation/screens/Home.spec.tsx
+++ tests/presentation/screens/Home.spec.tsx
@@ -5,89 +5,9 @@ import type { FitChart } from '@/domain/entities/types';
 import { UnexpectedError } from '@/domain/errors';
 import { Home } from '@/presentation/screens/Home';
 
-const makeSut = (
-  useCaseOutput: FitChart | Promise<never> | null = {
-    id: 'any_fit_chart_id',
-    userId: 'any_user_id',
-    goals: 'any_goal',
-    observation: 'any_observation',
-    divisions: [
-      { name: 'any_division_1', weekDays: [1, 2] },
-      { name: 'any_division_2', weekDays: [2, 4] },
-    ],
-    exercises: [
-      {
-        exerciseId: 'any_exercise_id_1',
-        name: 'any_exercise_name_1',
-        equipment: 'any_equipment_1',
-        series: 4,
-        repts: 10,
-        weight: 20,
-        division: 'any_division_1',
-        category: 'any_category_1',
-      },
-      {
-        exerciseId: 'any_exercise_id_2',
-        name: 'any_exercise_name_2',
-        series: 3,
-        repts: 12,
-        weight: 24,
-        division: 'any_division_1',
-        category: 'any_category_1',
-      },
-      {
-        exerciseId: 'any_exercise_id_3',
-        name: 'any_exercise_name_3',
-        equipment: 'any_equipment_3',
-        series: 3,
-        repts: 10,
-        weight: 30,
-        division: 'any_division_1',
-        category: 'any_category_1',
-      },
-      {
-        exerciseId: 'any_exercise_id_4',
-        name: 'any_exercise_name_4',
-        equipment: 'any_equipment_4',
-        series: 5,
-        repts: 10,
-        weight: 18,
-        division: 'any_division_1',
-        category: 'any_category_2',
-      },
-      {
-        exerciseId: 'any_exercise_id_5',
-        name: 'any_exercise_name_5',
-        equipment: 'any_equipment_5',
-        series: 4,
-        repts: 8,
-        weight: 26,
-        division: 'any_division_1',
-        category: 'any_category_2',
-      },
-      {
-        exerciseId: 'any_exercise_id_6',
-        name: 'any_exercise_name_6',
-        equipment: 'any_equipment_6',
-        series: 3,
-        repts: 10,
-        weight: 30,
-        division: 'any_division_2',
-        category: 'any_category_3',
-      },
-      {
-        exerciseId: 'any_exercise_id_7',
-        name: 'any_exercise_name_7',
-        equipment: 'any_equipment_7',
-        series: 3,
-        repts: 10,
-        weight: 30,
-        division: 'any_division_2',
-        category: 'any_category_3',
-      },
-    ],
-  },
-) => {
+import { mockFitChart } from '@/tests/mocks/domain/entitites';
+
+const makeSut = (useCaseOutput: FitChart | Promise<never> | null = mockFitChart()) => {
   const loadUserCurrentFitChartUsecase = jest.fn().mockResolvedValue(useCaseOutput);
   render(<Home loadUserCurrentFitChart={loadUserCurrentFitChartUsecase} />);
   waitFor(() => {
@@ -149,6 +69,14 @@ describe('Home', () => {
     const messageText = await screen.findByTestId('no-content-message');
 
     expect(messageText).toHaveTextContent('Você ainda não possui uma ficha de treino.');
+  });
+
+  it('Should present a message text if LoadUserCurrentFitChart returns a FitChart but there are no exercises for the current day', async () => {
+    makeSut({ ...mockFitChart(), divisions: [{ name: 'any_division', weekDays: [3] }] });
+
+    const messageText = await screen.findByTestId('no-content-message');
+
+    expect(messageText).toHaveTextContent('Você não possui exercicios para hoje.');
   });
 
   it('Should present fit chart on success', async () => {
