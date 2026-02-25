e6973b8f8fbc338f98f157f7c94e9dc9bfd4a22d
Allysson Alas <allyssonmgs@gmail.com>
2026-02-25 15:52:13 -0300

    feat: ensure Storage.get returns correct output on success

--- src/infra/gateways/storage.ts
+++ src/infra/gateways/storage.ts
@@ -2,13 +2,13 @@ import AsyncStorage from '@react-native-async-storage/async-storage';
 
 import type { GetStorage, SetStorage } from '@/domain/contracts/gateways';
 
-export class Storage implements SetStorage {
+export class Storage implements SetStorage, GetStorage {
   async set(input: SetStorage.Input): Promise<SetStorage.Output> {
     await AsyncStorage.setItem(input.key, JSON.stringify(input.value));
   }
 
   async get(input: GetStorage.Input): Promise<GetStorage.Output> {
-    await AsyncStorage.getItem(input.key);
-    return null;
+    const result = await AsyncStorage.getItem(input.key);
+    return result ? JSON.parse(result) : null;
   }
 }
