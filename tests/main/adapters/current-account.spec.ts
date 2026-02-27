b8a22807a7f708c726a5c4b2db88db7e05cae24d
Allysson Alas <allyssonmgs@gmail.com>
2026-02-27 09:06:59 -0300

    test: ensure getCurrentAccountAdapter calls makeStorageGateway().get with correct input

--- tests/main/adapters/current-account.spec.ts
+++ tests/main/adapters/current-account.spec.ts
@@ -1,15 +1,29 @@
 import { Storage } from '@/infra/gateways';
-import { setCurrentAccountAdapter } from '@/main/adapters';
+
+import { getCurrentAccountAdapter, setCurrentAccountAdapter } from '@/main/adapters';
 
 import { mockAuthedUser } from '@/tests/mocks/domain/entitites';
 
 describe('CurrentAccountAdapter', () => {
-  it('Should call Storage.set with correct input', async () => {
-    const setSpy = jest.spyOn(Storage.prototype, 'set');
+  describe('setCurrentAccountAdapter', () => {
+    it('Should call Storage.set with correct input', async () => {
+      const setSpy = jest.spyOn(Storage.prototype, 'set');
 
-    await setCurrentAccountAdapter(mockAuthedUser());
+      await setCurrentAccountAdapter(mockAuthedUser());
 
-    expect(setSpy).toHaveBeenCalledWith({ key: 'account', value: mockAuthedUser() });
-    expect(setSpy).toHaveBeenCalledTimes(1);
+      expect(setSpy).toHaveBeenCalledWith({ key: 'account', value: mockAuthedUser() });
+      expect(setSpy).toHaveBeenCalledTimes(1);
+    });
+  });
+
+  describe('getCurrentAccountAdapter', () => {
+    it('Should call Storage.get with correct input', async () => {
+      const getSpy = jest.spyOn(Storage.prototype, 'get');
+
+      await getCurrentAccountAdapter();
+
+      expect(getSpy).toHaveBeenCalledWith({ key: 'account' });
+      expect(getSpy).toHaveBeenCalledTimes(1);
+    });
   });
 });
