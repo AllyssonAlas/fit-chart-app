15e4822c2eebc2d262c48254c70f09c7f647a497
Allysson Alas <allyssonmgs@gmail.com>
2026-02-27 09:06:43 -0300

    feat: ensure getCurrentAccountAdapter calls makeStorageGateway().get with correct input

--- src/main/adapters/current-account.ts
+++ src/main/adapters/current-account.ts
@@ -4,3 +4,7 @@ import { makeStorageGateway } from '@/main/factories/infra/gateways';
 export const setCurrentAccountAdapter = async (account: AuthedUser) => {
   await makeStorageGateway().set({ key: 'account', value: account });
 };
+
+export const getCurrentAccountAdapter = async () => {
+  await makeStorageGateway().get({ key: 'account' });
+};
