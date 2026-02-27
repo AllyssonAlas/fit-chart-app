563563dce1eadc87b74ff06060d20ccf0f4dfa1d
Allysson Alas <allyssonmgs@gmail.com>
2026-02-27 09:10:31 -0300

    feat: ensure getCurrentAccountAdapter returns correct output on success

--- src/main/adapters/current-account.ts
+++ src/main/adapters/current-account.ts
@@ -5,6 +5,7 @@ export const setCurrentAccountAdapter = async (account: AuthedUser) => {
   await makeStorageGateway().set({ key: 'account', value: account });
 };
 
-export const getCurrentAccountAdapter = async () => {
-  await makeStorageGateway().get({ key: 'account' });
+export const getCurrentAccountAdapter = async (): Promise<AuthedUser | null> => {
+  const authedUser = await makeStorageGateway().get({ key: 'account' });
+  return authedUser;
 };
