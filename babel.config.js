6f7610e0c1366ef20a9de0e31b03dd2753595419
Allysson Alas <allyssonmgs@gmail.com>
2026-09-16 18:19:19 -0300

    build: add react-native-reactnimated

--- babel.config.js
+++ babel.config.js
@@ -10,15 +10,16 @@ module.exports = function (api) {
 
     // other config
     plugins: [
-        // other plugins
-        ['react-native-unistyles/plugin', {
-            // pass root folder of your application
-            // all files under this folder will be processed by the Babel plugin
-            // if you need to include more folders, or customize discovery process
-            // check available babel options
-            root: 'src'
-        }],
-        ['module:react-native-dotenv']
+      // other plugins
+      ['react-native-unistyles/plugin', {
+          // pass root folder of your application
+          // all files under this folder will be processed by the Babel plugin
+          // if you need to include more folders, or customize discovery process
+          // check available babel options
+          root: 'src'
+      }],
+      ['module:react-native-dotenv'],
+      'react-native-reanimated/plugin',
     ]
   }
 }
